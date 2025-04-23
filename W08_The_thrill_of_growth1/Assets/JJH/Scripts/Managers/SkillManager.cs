d22550faebf59690738f8d6ddb630598a7b5ad44
Jeon Jong Heon <122503945+skufmid@users.noreply.github.com>
2025-04-23 21:33:45 +0900

    [Feat] Enemy 스킬

--- W08_The_thrill_of_growth1/Assets/JJH/Scripts/Managers/SkillManager.cs
+++ W08_The_thrill_of_growth1/Assets/JJH/Scripts/Managers/SkillManager.cs
@@ -4,9 +4,24 @@ using UnityEngine.TextCore.Text;
 
 public class SkillManager : MonoBehaviour
 {
+
+    private static SkillManager _instance;
+    public static SkillManager Instance => _instance;
+
     SkillComponent skillComponent;
+
     float value;
     float coefficient;
+
+    private void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = this;
+            DontDestroyOnLoad(gameObject);
+            Init();
+        }
+    }
     public void Init()
     {
         skillComponent = GetComponent<SkillComponent>();
@@ -14,65 +29,48 @@ public class SkillManager : MonoBehaviour
 
     public void InvokeSkill(Unit unit, int skillId)
     {
-    
-
         SkillSO skill = Array.Find(Manager.Data.Skills, s => s.Id == skillId);
         if (skillId >= 100) return;
 
         Character character = unit.GetComponent<Character>();
-        //coefficient = skill.
+        coefficient = Manager.Data.Skills[skillId].Coefficients[character.Star];
         switch (skillId)
         {
             case 0:
                 value = unit.MaxHp;
-                switch (character.Star)
-                {
-                    case 1:
-                        coefficient = 0.3f;
-                        break;
-                    case 2:
-                        coefficient = 0.35f;
-                        break;
-                    case 3:
-                        coefficient = 0.4f;
-                        break;
-                    case 4:
-                        coefficient = 0.45f;
-                        break;
-                }
-                skillComponent.DamageSkill(Manager.Battle.GetRandomEnemy(1), value * coefficient);
+                skillComponent.DamageSkill(Manager.Battle.GetRandomEnemy(1), value * coefficient / 100);
                 break;
 
             case 1:
                 float HpRatio = unit.Hp / unit.MaxHp;
-                if (HpRatio >= 0.8f)
-                {
-                    //coefficient
-                }
-                else
+                if (HpRatio < 0.8f)
                 {
                     coefficient = 1.00f;
                 }
-                skillComponent.DamageSkill(Manager.Battle.GetRandomEnemy(1), unit.Damage * coefficient);
+                skillComponent.DamageSkill(Manager.Battle.GetRandomEnemy(1), unit.Damage * coefficient / 100);
                 break;
 
             case 2:
-                //coeffiecint
-                skillComponent.DamageSkill(Manager.Battle.GetRandomEnemy(3), unit.Damage * coefficient);
+                skillComponent.DamageSkill(Manager.Battle.GetRandomEnemy(3), unit.Damage * coefficient / 100);
                 break;
 
             case 3:
-                //coeffiecient
-
                 GameObject[] Targets = Manager.Battle.characterList.ToArray();
                 skillComponent.ApplyEffectEnemyPercentSkill(Targets, EStat.AttackSpeed, null, EStat.AttackSpeed, coefficient);
                 break;
 
             case 4:
-
+                Targets = new GameObject[1];
+                Targets[0] = unit.gameObject;
+                value = unit.MaxHp;
+                skillComponent.ApplyEffectAmountSkill(Targets, EStat.Hp, null, value * coefficient);
                 break;
-                //skillComponent
-
         }
+    }
+
+    public void InvokeEnemySkill(Enemy enemy)
+    {
+        value = enemy.Damage;
+        skillComponent.DamageSkill(new GameObject[] { Manager.Battle.GetTargetByPositionPriority() }, value);
     }
 }
