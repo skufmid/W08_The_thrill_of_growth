d76a8455d638bf07898a3adebd8641441738d383
Breadmannn <you4521@naver.com>
2025-04-24 08:38:02 +0900

    [feat] 0837 0424 오브시스템

--- W08_The_thrill_of_growth1/Assets/YBH/Scripts/Orbs/Orb.cs
+++ W08_The_thrill_of_growth1/Assets/YBH/Scripts/Orbs/Orb.cs
@@ -21,10 +21,13 @@ private void Awake()
 public void OnBeginDrag(PointerEventData eventData)
 {
     canvasGroup.blocksRaycasts = false;
-}
+        string desc = GetTooltipDescription();
+        TooltipManager.Instance.Show(desc, eventData.position);
+    }
 
     public void OnDrag(PointerEventData eventData)
     {
+
         Vector2 localPoint;
         RectTransformUtility.ScreenPointToLocalPointInRectangle(
             canvas.transform as RectTransform,
@@ -34,9 +37,37 @@ public void OnBeginDrag(PointerEventData eventData)
         );
 
         rectTransform.anchoredPosition = localPoint;
+        TooltipManager.Instance.Show(GetTooltipDescription(), eventData.position);
     }
     public void OnEndDrag(PointerEventData eventData)
-{
+    {
     canvasGroup.blocksRaycasts = true;
-}
+        TooltipManager.Instance.Hide();
+
+    }
+    private string GetTooltipDescription()
+    {
+        switch (orbType)
+        {
+            case OrbType.Damage:
+                return $"공격력 +{value}";
+            case OrbType.AttackSpeed:
+                return $"공격속도 +{value:F2}";
+            case OrbType.MaxHP:
+                return $"최대 체력 +{value}";
+            case OrbType.ManaGain:
+                return $"마나 회복량 +{value}";
+            case OrbType.Potion:
+                return $"HP 회복 +{value}%";
+            case OrbType.ManaPotion:
+                return $"MP 회복 +{value}%";
+            default:
+                return "알 수 없는 오브";
+        }
+    }
+    private void OnDestroy()
+    {
+        if (TooltipManager.Instance != null)
+            TooltipManager.Instance.Hide();
+    }
 }
