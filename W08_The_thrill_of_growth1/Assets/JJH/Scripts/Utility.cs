272b38c1d15adadc696d589c2217d82542b0b61c
Jeon Jong Heon <122503945+skufmid@users.noreply.github.com>
2025-04-23 14:08:10 +0900

    [Feat] 더미 데이터 생성

--- W08_The_thrill_of_growth1/Assets/JJH/Scripts/Utility.cs
+++ W08_The_thrill_of_growth1/Assets/JJH/Scripts/Utility.cs
@@ -2,9 +2,17 @@ using UnityEngine;
 
 public enum EAlliance
 {
+    Kingdom,
+    Northward,
+    Dark,
+    HolyLight,
 }
 
 public enum EClass
 {
+    Tanker,
+    Warrior,
+    Wizard,
+    Archer,
 }
 
